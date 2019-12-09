175ce148eb15d52d3afb16d3f0d3dee2de1af565
Iorran Castro <iorran.castro@vakt.io>
2019-12-09 02:12:41 +0000

    minimal setup for tests

---
+++ src/web/components/Button/Button2.test.tsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import Button from './Button.styles';
+
+test('renders learn react link', () => {
+  expect(1).toBe(1);
+  // const { getByText } = render(<Button onClick={() => {}}>2</Button>);
+});
